24d9d2008baba35ee660af44fbb038f3311c5951
Paige Venuto <paige.venuto@charter.com>
2022-07-07 15:02:38 -0400

    made small progress with mongo script

--- initdb.js
+++ initdb.js
@@ -1,20 +1,42 @@
-const schema = {
-  bsonType: "object",
-  additionalProperties: false,
-  title: "part",
-  required: ["partno", "name", "vendor", "count", "location", "category"],
-  properties: {
-    _id: { bsonType: "objectId" },
-    name: { bsonType: "string" },
-    partno: { bsonType: "string" },
-    vendor: { bsonType: "string" },
-    count: { bsonType: "number" },
-    location: { bsonType: "string" },
-    sublocation: { bsonType: "string" },
-    description: { bsonType: "string" },
-    serialno: { bsonType: "string" },
-    category: { bsonType: "string" },
-    subcategory: { bsonType: "string" },
-    notes: { bsonType: "string" },
+// TODO create database and with auth for app
+
+db.createCollection("assets", { validator: assetValidator });
+db.createCollection("users", { validator: userValidator });
+
+// TODO create userValidator
+// TODO double check assetValidator when less tired
+
+const assetValidator = {
+  $jsonSchema: {
+    bsonType: "object",
+    additionalProperties: false,
+    title: "part",
+    required: ["partno", "name", "vendor", "count", "location", "category"],
+    properties: {
+      _id: { bsonType: "objectId" },
+      name: { bsonType: "string" },
+      partno: { bsonType: "string" },
+      vendor: { bsonType: "string" },
+      count: { bsonType: "number" },
+      location: { bsonType: "string" },
+      sublocation: { bsonType: "string" },
+      description: { bsonType: "string" },
+      serialno: { bsonType: "string" },
+      category: { bsonType: "string" },
+      subcategory: { bsonType: "string" },
+      notes: { bsonType: "string" },
+    },
   },
 };
+
+db.users.insert({
+  username: "admin",
+  password: "admin",
+  permissions: {
+    admin: true,
+    create: true,
+    read: true,
+    update: true,
+    delete: true,
+  },
+});
