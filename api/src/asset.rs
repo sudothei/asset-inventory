8594695fa732329b4bf99e258a06dc44620a8bbc
Paige Venuto <paige.venuto@charter.com>
2022-07-13 12:25:09 -0400

    one route done, several to go

--- api/src/asset.rs
+++ api/src/asset.rs
@@ -1,6 +1,9 @@
 use actix_web::web::Json;
-use actix_web::{post, HttpResponse};
+use actix_web::{get, post, web, HttpResponse, Responder};
+use mongodb::{bson::doc, Database};
 use serde::{Deserialize, Serialize};
+use std::sync::*;
+use tokio_stream::StreamExt;
 
 #[derive(Deserialize, Serialize)]
 pub struct AssetRequest {
@@ -17,15 +20,36 @@ pub struct AssetRequest {
     pub notes: Option<String>,
 }
 
-// list all assets `/assets`
-//#[get("/assets")]
-//pub async fn list() -> HttpResponse {
-//// TODO
-//let assets = {};
-//HttpResponse::Ok()
-//.content_type("application/json")
-//.json(assets)
-//}
+#[derive(Serialize, Deserialize)]
+pub struct Asset {
+    pub _id: i32,
+    pub name: String,
+    pub assetno: String,
+    pub vendor: String,
+    pub category: String,
+    pub subcategory: Option<String>,
+    pub count: i32,
+    pub location: String,
+    pub sublocation: Option<String>,
+    pub description: Option<String>,
+    pub serialno: Option<String>,
+    pub notes: Option<String>,
+}
+
+/// list all assets `/assets`
+#[get("/assets")]
+pub async fn list(data: web::Data<Mutex<Database>>) -> impl Responder {
+    let db = data.lock().unwrap();
+    let asset_collection = db.collection::<Asset>("assets");
+    let cursor = asset_collection.find(None, None).await.unwrap();
+
+    let results: Vec<Result<Asset, mongodb::error::Error>> = cursor.collect().await;
+    let assets: Vec<Asset> = results.into_iter().map(|d| d.unwrap()).collect();
+
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .json(assets)
+}
 
 /// create a asset `/assets`
 #[post("/assets")]
